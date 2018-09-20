c5056c93ae3676a5b033a5fe8a35c181a1f26150
Chaojie Zhu <zackspbc@gmail.com>
2018-09-19 20:48:57 -0400

    Doxygen time

--- Isetta/Isetta/Core/Time/Clock.h
+++ Isetta/Isetta/Core/Time/Clock.h
@@ -26,14 +26,35 @@ class Clock {
   Clock& operator=(const Clock& inClock) = delete;
   Clock& operator=(Clock&& inClock) = delete;
 
+  /**
+   * \brief The time scale of this clock
+   */
   float timeScale;
+  /**
+   * \brief Check if the clock is paused
+   */
   bool isPause;
 
+  /**
+   * \brief Update the clock time by realtime
+   */
   void UpdateTime();
+  /**
+   * \brief Get the delta time from last update
+   */
   double GetDeltaTime() const;
+  /**
+   * \brief Get elapsed time since the clock started
+   */
   double GetElapsedTime() const;
+  /**
+   * \brief Get unscaled elapsed time since clock started
+   */
   double GetElapsedUnscaledTime() const;
 
+  /**
+   * \brief Get the real timestamp
+   */
   static U64 GetTimestamp();
 };
 
