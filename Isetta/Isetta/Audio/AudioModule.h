88dcf30d209fbce7a589ea067c1e68d33819558d
Yidi-Z <zhuyidiwow@gmail.com>
2018-09-23 21:40:41 -0400

    Doxygen Audio

--- Isetta/Isetta/Audio/AudioModule.h
+++ Isetta/Isetta/Audio/AudioModule.h
@@ -8,8 +8,8 @@
 #include "fmod.hpp"
 // TODO(YIDI): do error checking
 // #include "fmod_errors.h"
-#include "Core/ModuleManager.h"
 #include "Core/IsettaAlias.h"
+#include "Core/ModuleManager.h"
 
 namespace Isetta {
 
@@ -18,12 +18,43 @@ class AudioModule {
   AudioModule() = default;
   ~AudioModule() = default;
 
+  /**
+   * \brief Start up the Audio module. Initialize the FMOD::System and read all
+   * sound files
+   */
   void StartUp();
+
+  /**
+   * \brief Read all sound files as specified in the config file into memory,
+   * and store them into a unordered_map for later retrieval
+   */
   void LoadAllAudioClips();
+
+  /**
+   * \brief Update the Audio module by ticking FMOD::System
+   * \param deltaTime 
+   */
   void Update(float deltaTime) const;
+
+  /**
+   * \brief Shut down the Audio module. Stop all sound and free all memory
+   */
   void ShutDown();
 
+  /**
+   * \brief Get a handle to the sound
+   * \param soundName name of the sound file you want to find
+   * \return 
+   */
   FMOD::Sound* FindSound(const char* soundName);
+
+  /**
+   * \brief Play the FMOD::Sound with the specified properties
+   * \param sound 
+   * \param loop 
+   * \param volume 
+   * \return 
+   */
   FMOD::Channel* Play(FMOD::Sound* sound, bool loop, float volume) const;
 
   FMOD::System* fmodSystem;
