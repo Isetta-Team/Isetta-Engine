0175e9553fbb261a0998161d1b72a9390ff27c80
Yidi-Z <zhuyidiwow@gmail.com>
2018-10-03 15:53:46 -0400

    Added new array utilities

--- Isetta/Isetta/Core/Memory/MemoryManager.h
+++ Isetta/Isetta/Core/Memory/MemoryManager.h
@@ -5,13 +5,13 @@
 #include "Core/Config/CVar.h"
 #include "Core/IsettaAlias.h"
 #include "Core/Memory/DoubleBufferedAllocator.h"
-#include "Core/Memory/ObjectHandle.h"
-#include "Core/Memory/StackAllocator.h"
 #include "Core/Memory/FreeListAllocator.h"
 #include "Core/Memory/MemUtil.h"
+#include "Core/Memory/ObjectHandle.h"
+#include "Core/Memory/StackAllocator.h"
 
 namespace Isetta {
-
+// TODO(YIDI): Alloc all allocators on a big stack
 class MemoryManager {
  public:
   struct MemoryConfig {
@@ -22,17 +22,6 @@ class MemoryManager {
     CVar<Size> dynamicArenaSize{"dynamic_arena_size", 10_MB};
     CVar<Size> freeListAllocatorSize{"free_list_allocator_size", 10_MB};
   };
-
-  /**
-   * \brief Create a new object whose memory is going to be cleared at the end
-   * of this frame. You need to manually call destructor on that object if
-   * needed
-   *
-   * \tparam T type of object to create
-   * \return
-   */
-  template <typename T, typename... args>
-  static T* NewOnSingleFrame(args...);
 
   /**
    * \brief Grab some properly aligned free memory to use. The memory will be
@@ -48,13 +37,17 @@ class MemoryManager {
 
   /**
    * \brief Create a new object whose memory is going to be cleared at the end
-   * of the next frame. You need to manually call destructor on that object if
+   * of this frame. You need to manually call destructor on that object if
    * needed
    *
-   * \tparam T type of object to create \return
+   * \tparam T type of object to create
+   * \return
    */
   template <typename T, typename... args>
-  static T* NewOnDoubleBuffered(args...);
+  static T* NewOnSingleFrame(args...);
+
+  template<typename T>
+  static T* NewArrOnSingleFrame(Size length, U8 alignment = MemUtil::ALIGNMENT);
 
   /**
    * \brief Grab some properly aligned free memory to use. The memory will be
@@ -66,22 +59,43 @@ class MemoryManager {
    *
    * \return
    */
-  static void* AllocOnDoubleBuffered(Size size, U8 alignment = MemUtil::ALIGNMENT);
+  static void* AllocOnDoubleBuffered(Size size,
+                                     U8 alignment = MemUtil::ALIGNMENT);
+
+  /**
+   * \brief Create a new object whose memory is going to be cleared at the end
+   * of the next frame. You need to manually call destructor on that object if
+   * needed
+   *
+   * \tparam T type of object to create \return
+   */
+  template <typename T, typename... args>
+  static T* NewOnDoubleBuffered(args...);
+
+  template<typename T>
+  static T* NewArrOnDoubleBuffered(Size length, U8 alignment = MemUtil::ALIGNMENT);
 
   // the memory will either from LSR - when starting up the engine
   // or LevelData - after engine startup
   // make sure you don't put data in wrong places
   static void* AllocOnStack(Size size, U8 alignment = MemUtil::ALIGNMENT);
 
-  static void* AllocOnFreeList(Size size, U8 alignment = MemUtil::ALIGNMENT);
+  template <typename T, typename... args>
+  static T* NewOnStack(args...);
 
-  static void FreeOnFreeList(void* memPtr);
+  template <typename T>
+  static T* NewArrOnStack(Size length, U8 alignment = MemUtil::ALIGNMENT);
+
+  // TODO(YIDI): Use different freelist allocators for different stage
+  static void* AllocOnFreeList(Size size, U8 alignment = MemUtil::ALIGNMENT);
 
   template <typename T, typename... args>
   static T* NewOnFreeList(args... argList);
 
-  template<typename T>
+  template <typename T>
   static T* NewArrOnFreeList(Size length, U8 alignment = MemUtil::ALIGNMENT);
+
+  static void FreeOnFreeList(void* memPtr);
 
   /**
    * \brief Create an object that will sit on the dynamic memory area. You need
@@ -157,12 +171,32 @@ T* MemoryManager::NewOnSingleFrame(args... argList) {
   return GetInstance()->singleFrameAllocator.New<T>(argList...);
 }
 
+template <typename T>
+T* MemoryManager::NewArrOnSingleFrame(const Size length, const U8 alignment) {
+  return GetInstance()->singleFrameAllocator.NewArr<T>(length, alignment);
+}
+
 template <typename T, typename... args>
 T* MemoryManager::NewOnDoubleBuffered(args... argList) {
   return GetInstance()->doubleBufferedAllocator.New<T>(argList...);
 }
 
-template <typename T, typename ... args>
+template <typename T>
+T* MemoryManager::NewArrOnDoubleBuffered(const Size length, const U8 alignment) {
+  return GetInstance()->doubleBufferedAllocator.NewArr<T>(length, alignment);
+}
+
+template <typename T, typename... args>
+T* MemoryManager::NewOnStack(args... argList) {
+  return GetInstance()->lsrAndLevelAllocator.New<T>(argList...);
+}
+
+template <typename T>
+T* MemoryManager::NewArrOnStack(const Size length, const U8 alignment) {
+  return GetInstance()->lsrAndLevelAllocator.NewArr<T>(length, alignment);
+}
+
+template <typename T, typename... args>
 T* MemoryManager::NewOnFreeList(args... argList) {
   return GetInstance()->freeListAllocator.New<T>(argList...);
 }
