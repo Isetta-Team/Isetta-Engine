c2e4e77dce13bdb52f8c9376728350410ab0d872
cbiasco <biasco.caleb@gmail.com>
2018-11-20 23:42:53 -0500

    Fixed Component FixedUpdate initialization

--- Isetta/IsettaEngine/Scene/Component.cpp
+++ Isetta/IsettaEngine/Scene/Component.cpp
@@ -56,7 +56,7 @@ void Component::FlattenHelper(std::type_index parent, std::type_index curr) {
 }
 
 Component::Component()
-    : attributes{0b111001}, entity{curEntity}, transform(curTransform) {
+    : attributes{0b1111001}, entity{curEntity}, transform(curTransform) {
   if (!isFlattened) {
     FlattenComponentList();
   }
