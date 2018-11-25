940fd8369a35d48d7990e293ca995770a1322478
Chaojie Zhu <zackspbc@gmail.com>
2018-11-24 22:45:31 -0500

    Clearing engine resources

---
+++ Isetta/IsettaEngine/Core/EngineResource.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 Isetta
+ */
+#pragma once
+#include <string>
+
+namespace Isetta {
+struct EngineResource {
+  static std::string defaultLightMat;
+  static std::string defaultParticle;
+};
+}
