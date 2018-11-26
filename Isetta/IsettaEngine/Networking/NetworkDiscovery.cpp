489f5eb0be0c49bb323b91ca44f090fc1424910e
cbiasco <biasco.caleb@gmail.com>
2018-11-26 00:29:04 -0500

    Fixed merge error

--- Isetta/IsettaEngine/Networking/NetworkDiscovery.cpp
+++ Isetta/IsettaEngine/Networking/NetworkDiscovery.cpp
@@ -207,8 +207,4 @@ void NetworkDiscovery::BroadcastMessage(std::string_view message) const {
     LOG_ERROR(Debug::Channel::Networking, "Send failed");
   }
 }
-
-bool NetworkDiscovery::IsBroadcasterRunning() const {
-  return broadcasterSocket != -1;
-}
 }  // namespace Isetta
